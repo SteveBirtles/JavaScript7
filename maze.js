8007ef9da21d7dce76526ba7834b5707bd39f6af
Steve Birtles <sbirtles@farnborough.ac.uk>
2019-11-19 16:06:58 +0000

    Add files via upload

--- maze.js
+++ maze.js
@@ -1,26 +1,22 @@
 const mazeWidth = 36, mazeHeight = 20, wallWidth = 6;
-let w = 0, h = 0, squareSize = 0;
-const pressedKeys = {};
+let maze, mazeDone = false, squareSize = 0;
+
+let w = 0, h = 0;
 let lastTimestamp = 0;
 
 let myX = 0;
 let myY = 0;
+const pressedKeys = {};
 
-let maze = [];
-let mazeDone = false;
+function fixSize() {
 
-function resetMaze() {
-    maze = [];
-    let n = 0;
-    for (let i = 0; i < mazeWidth; i++) {
-        let column = [];
-        for (let j = 0; j < mazeHeight; j++) {
-            column.push({down: false, right:  i < false, set: n, colour: 'grey'});
-            n++;
-        }
-        maze.push(column);
-    }
-    mazeDone = false;
+    w = window.innerWidth;
+    h = window.innerHeight;
+    const canvas = document.getElementById('mazeCanvas');
+    canvas.width = w;
+    canvas.height = h;
+    squareSize = h / (mazeHeight + 1);
+
 }
 
 function pageLoad() {
@@ -37,121 +33,21 @@ function pageLoad() {
 
 }
 
-function fixSize() {
-
-    w = window.innerWidth;
-    h = window.innerHeight;
-    const canvas = document.getElementById('mazeCanvas');
-    canvas.width = w;
-    canvas.height = h;
-    squareSize = h / (mazeHeight + 1);
-
-}
-
-function gameFrame(timestamp) {
-
-    if (lastTimestamp === 0) lastTimestamp = timestamp;
-    const frameLength = (timestamp - lastTimestamp) / 1000;
-    lastTimestamp = timestamp;
-
-    inputs();
-    processes(frameLength);
-    outputs();
-
-    window.requestAnimationFrame(gameFrame);
-
-}
-
-
-function inputs() {
-
-    if (pressedKeys["ArrowUp"]) {
-        if (!keyDown && myY > 0 && maze[myX][myY-1].down) {
-            myY--;
-            keyDown = true;
-        }
-    } else if (pressedKeys["ArrowDown"]) {
-        if (!keyDown && myY < mazeHeight-1 && maze[myX][myY].down) {
-            myY++;
-            keyDown = true;
-        }
-    } else if (pressedKeys["ArrowLeft"]) {
-        if (!keyDown && myX > 0 && maze[myX-1][myY].right) {
-            myX--;
-            keyDown = true;
-        }
-    } else if (pressedKeys["ArrowRight"]) {
-        if (!keyDown && myX < mazeWidth-1 && maze[myX][myY].right) {
-            myX++;
-            keyDown = true;
-        }
-    } else if (pressedKeys["g"]) {
-        if (!keyDown) {
-            resetMaze();
-            keyDown = true;
-        }
-    } else if (pressedKeys["f"]) {
-        if (!keyDown) {
-            while (!mazeDone) {
-                mazeDone = mazeStep();
-            }
-            keyDown = true;
-        }
-    } else {
-        keyDown = false;
-    }
-
-}
-
-function processes(frameLength) {
-
-    if (!mazeDone) {
-        mazeDone = mazeStep();
-    }
-
-}
-
-function outputs() {
-
-    const canvas = document.getElementById('mazeCanvas');
-    const context = canvas.getContext('2d');
-
-    context.fillStyle = 'navy';
-    context.fillRect(0,0,w,h);
-
+function resetMaze() {
+    maze = [];
+    let n = 0;
     for (let i = 0; i < mazeWidth; i++) {
-      for (let j = 0; j < mazeHeight; j++) {
-
-        let x = w/2 - (mazeWidth*squareSize)/2 + i*squareSize;
-        let y = h/2 - (mazeHeight*squareSize)/2 + j*squareSize;
-
-        context.fillStyle = maze[i][j].colour;
-        context.fillRect(x + wallWidth, y + wallWidth, squareSize - 2*wallWidth, squareSize - 2*wallWidth);
-        if (maze[i][j].down) context.fillRect(x + wallWidth, y + squareSize - wallWidth - 1, squareSize - 2*wallWidth, wallWidth * 2 + 2);
-        if (maze[i][j].right) context.fillRect(x + squareSize - wallWidth - 1, y + wallWidth, wallWidth * 2 + 2, squareSize - 2*wallWidth);
-
-        if (!mazeDone) {
-            context.fillStyle = 'black';
-            context.font = '15px Arial';
-            context.textAlign = 'left';
-            context.textBaseline = 'top';
-            context.fillText(maze[i][j].set, x + wallWidth + 2, y + wallWidth + 2);
+        let column = [];
+        for (let j = 0; j < mazeHeight; j++) {
+            column.push({down: false, right:  i < false, set: n, colour: 'grey'});
+            n++;
         }
-
-        if (myX == i && myY == j) {
-            context.fillStyle = 'red';
-            context.beginPath();
-            context.arc(x + squareSize/2, y + squareSize/2, squareSize/3, 0, 2*Math.PI);
-            context.fill();
-        }
-
-      }
+        maze.push(column);
     }
-
-
+    mazeDone = false;
 }
 
-function mazeStep() {
+function kruskalStep() {
 
     let oneSet = true;
     for (let i = 0; i < mazeWidth; i++) {
@@ -204,6 +100,108 @@ function mazeStep() {
 
         return false;
 
+    }
+
+}
+
+function gameFrame(timestamp) {
+
+    if (lastTimestamp === 0) lastTimestamp = timestamp;
+    const frameLength = (timestamp - lastTimestamp) / 1000;
+    lastTimestamp = timestamp;
+
+    inputs();
+    processes(frameLength);
+    outputs();
+
+    window.requestAnimationFrame(gameFrame);
+
+}
+
+
+function inputs() {
+
+    if (pressedKeys["ArrowUp"]) {
+        if (!keyDown && myY > 0 && maze[myX][myY-1].down) {
+            myY--;
+            keyDown = true;
+        }
+    } else if (pressedKeys["ArrowDown"]) {
+        if (!keyDown && myY < mazeHeight-1 && maze[myX][myY].down) {
+            myY++;
+            keyDown = true;
+        }
+    } else if (pressedKeys["ArrowLeft"]) {
+        if (!keyDown && myX > 0 && maze[myX-1][myY].right) {
+            myX--;
+            keyDown = true;
+        }
+    } else if (pressedKeys["ArrowRight"]) {
+        if (!keyDown && myX < mazeWidth-1 && maze[myX][myY].right) {
+            myX++;
+            keyDown = true;
+        }
+    } else if (pressedKeys["g"]) {
+        if (!keyDown) {
+            resetMaze();
+            keyDown = true;
+        }
+    } else if (pressedKeys["f"]) {
+        if (!keyDown) {
+            while (!mazeDone) {
+                mazeDone = kruskalStep();
+            }
+            keyDown = true;
+        }
+    } else {
+        keyDown = false;
+    }
+
+}
+
+function processes(frameLength) {
+
+    if (!mazeDone) {
+        mazeDone = kruskalStep();
+    }
+
+}
+
+function outputs() {
+
+    const canvas = document.getElementById('mazeCanvas');
+    const context = canvas.getContext('2d');
+
+    context.fillStyle = 'navy';
+    context.fillRect(0,0,w,h);
+
+    for (let i = 0; i < mazeWidth; i++) {
+      for (let j = 0; j < mazeHeight; j++) {
+
+        let x = w/2 - (mazeWidth*squareSize)/2 + i*squareSize;
+        let y = h/2 - (mazeHeight*squareSize)/2 + j*squareSize;
+
+        context.fillStyle = maze[i][j].colour;
+        context.fillRect(x + wallWidth, y + wallWidth, squareSize - 2*wallWidth, squareSize - 2*wallWidth);
+        if (maze[i][j].down) context.fillRect(x + wallWidth, y + squareSize - wallWidth - 1, squareSize - 2*wallWidth, wallWidth * 2 + 2);
+        if (maze[i][j].right) context.fillRect(x + squareSize - wallWidth - 1, y + wallWidth, wallWidth * 2 + 2, squareSize - 2*wallWidth);
+
+        if (!mazeDone) {
+            context.fillStyle = 'black';
+            context.font = '15px Arial';
+            context.textAlign = 'left';
+            context.textBaseline = 'top';
+            context.fillText(maze[i][j].set, x + wallWidth + 2, y + wallWidth + 2);
+        }
+
+        if (myX == i && myY == j) {
+            context.fillStyle = 'red';
+            context.beginPath();
+            context.arc(x + squareSize/2, y + squareSize/2, squareSize/3, 0, 2*Math.PI);
+            context.fill();
+        }
+
+      }
     }
 
 }
